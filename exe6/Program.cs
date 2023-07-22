464ad9960238346a4eb911c67bc187e30077bbae
«Elena <leseee4ka@bk.ru>
2023-07-22 15:03:03 +0300

    exe6

---
+++ exe6/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка)
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine() ?? "";
+int even = int.Parse(number);
+
+if (even % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("не четное");
+}
